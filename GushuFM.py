8e0ed12d151dd1bb6103bb80db5595eea49884da
knutmerket <dodraugen2k@hotmail.com>
2019-06-23 13:19:17 +0200

    First version of script. Will download all the episodes of GushiFM and store them in a folder with that name. Currently not able to check for episodes already stored in the folder, i.e. will always download all episodes.

---
+++ GushuFM.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 18:30:55 2019
+
+@author: knut
+"""
+
+import os
+import requests
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+
+#Set exist_ok to False before finishing... Inside a try/except statement that handles the exception that is raised when the folder exists?
+os.makedirs('./GushiFM', exist_ok=True)
+os.chdir('./GushiFM')
+
+
+page_count = 1
+
+episode_list = []
+
+# Get a list of all the episodes
+while True:
+    driver.get('http://storyfm.cn/episodes/page/' + str(page_count))
+    if len(driver.find_elements_by_css_selector("a.soundbyte-podcast-play-progression")) == 0:
+        break
+    else:
+        for i in driver.find_elements_by_css_selector("a.soundbyte-podcast-play-progression"):
+            episode_list.append(i.get_attribute('href'))
+    page_count += 1
+    
+print(episode_list)
+print("List with a total of %s links from %d pages created" % (len(episode_list), page_count-1))
+
+#Uncomment below lines marked "MP3-URLs" if you want a list of the direct URLs to the MP3s to be printed at the end
+#direct_links = [] #MP3-URLs
+
+for link in episode_list:
+    driver.get(link)
+    try:
+        audio_link_raw = driver.find_element_by_css_selector('audio').get_attribute('src')
+        print('Placeholder 1')
+        # Remove part after ".mp3" from link
+        audio_link = audio_link_raw.split('?')[0]
+        #direct_links.append(audio_link) #MP3-URLs
+        file_name = audio_link.split('/')[-1]
+        res = requests.get(audio_link)
+        res.raise_for_status()
+        save_file = open(file_name, 'wb')
+        for chunk in res.iter_content(100000):
+            save_file.write(chunk)
+    except:
+        print('Problem!')
+        continue
+    
+print('All done!')
+    
+#if len(direct_links) > 0: #MP3-URLs
+    #print(direct_links) #MP3-URLs
+ 
